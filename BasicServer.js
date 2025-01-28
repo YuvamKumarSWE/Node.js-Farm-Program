a5a46820ac1dad46640ea556fcc03ebcc9776922
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-01-28 13:01:50 -0800

    Basic Server update

--- BasicServer.js
+++ BasicServer.js
@@ -1,44 +1,33 @@
-const http = require('http'); //Http module
-// Read the content of input.txt
-//fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//        console.error('Error reading file:', err.message);
- //       return;
-//    }
-//    console.log('File Content:', data);
-//});
-require('url');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-/*const server=
-    //Create server {Callback function that runs when request hits }
-    http.createServer((req, res) => {
-    console.log(req.url); //info on request
+//sync code runs only once at the start
+const data = fs.readFileSync('./dev-data/data.json', 'utf8');
+const dataObj = JSON.parse(data);
 
-        const pathName = req.url;
-        if(pathName === '/' || pathName === '/overview'){
-            res.end('overview');
-        }
-        else if(pathName === '/product'){
-            res.end('product');
-        }
-        else{
-            res.writeHead(404, {
-                'Content-Type': 'text/html',
-                'my-own-header': 'hello - world'
+const server = http.createServer((req, res) => {
+    const pathname = req.url;
 
+    switch (pathname) {
+        case '/':
+            res.end('Hello World!');
+            break
+        case '/overview':
+            res.end('Overview');
+            break;
+        case '/api':
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(data);
+            break;
+        default:
+            res.writeHead(404, {'content-type': 'text/html',
+                'my-own-header' : "Hello World!"
             });
-            res.end('<h1>404</h1>');
-        }
-
-    // Response object
-    //res.end('Hello World!'); //send back simple response
+            res.end('<h1>Not Found!</h1>');
+    }
 })
 
-//Listen to requests
-server.listen(8000, '127.0.0.1', () =>{
-    console.log('Listening on port 8000'); //Callback function when server starts running
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server started!');
 });
-
-// event loop keeps running and won't exit program
-// Routing to react to different urls (express would simplify)
-*/
